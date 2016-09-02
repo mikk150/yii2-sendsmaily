7cda853047dbe37883d92c5663694b27ea56f039
Mikk Tendermann <mikk150@gmail.com>
2016-09-02 11:33:25 +0300

    set whole bunch of methods that are not really needed

--- src/Message.php
+++ src/Message.php
@@ -13,6 +13,8 @@ class Message extends BaseMessage
 
     private $_templateParams;
 
+    private $_to;
+
     public function setTemplateId($value)
     {
         $this->_templateId = $value;
@@ -31,5 +33,90 @@ class Message extends BaseMessage
     public function getTemplateParams()
     {
         return $this->_templateParams;
+    }
+
+    public function getCharset()
+    {
+        # code...
+    }
+    public function setCharset($v)
+    {
+        # code...
+    }
+    public function getFrom()
+    {
+        # code...
+    }
+    public function setFrom($v)
+    {
+        # code...
+    }
+    public function getTo()
+    {
+        return $this->_to;
+    }
+    public function setTo($to)
+    {
+        $this->_to = $to;
+    }
+    public function getReplyTo()
+    {
+        # code...
+    }
+    public function setReplyTo($v)
+    {
+        # code...
+    }
+    public function getCc()
+    {
+        # code...
+    }
+    public function setCc($v)
+    {
+        # code...
+    }
+    public function getBcc()
+    {
+        # code...
+    }
+    public function setBcc($v)
+    {
+        # code...
+    }
+    public function getSubject()
+    {
+        # code...
+    }
+    public function setSubject($v)
+    {
+        # code...
+    }
+    public function setTextBody($v)
+    {
+        # code...
+    }
+    public function setHtmlBody($v)
+    {
+        # code...
+    }
+    public function attach($fileName, array $options = [])
+    {
+        # code...
+    }
+    public function attachContent($fileName, array $options = [])
+    {
+        # code...
+    }
+    public function embed($fileName, array $options = [])
+    {
+        # code...
+    }
+    public function embedContent($fileName, array $options = [])
+    {
+        # code...
+    }
+    public function toString()
+    {
+        # code...
     }
 }
